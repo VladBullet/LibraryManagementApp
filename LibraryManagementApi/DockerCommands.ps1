ed2fa5b1800d4d42578e5a288acd9120969b6192
Vlad Bulete <vlad.bulete@proplanet.com.ro>
2023-05-16 11:50:43 +0300

    added docker commands file and connectionString for postgresql database

---
+++ LibraryManagementApi/DockerCommands.ps1
@@ -0,0 +1,4 @@
+﻿# Run postgresql docker 
+docker run --name postgres-library-db -p 5455:5432 -e POSTGRES_USER=postgresUser -e POSTGRES_PASSWORD=my5ecr3tqa55w0rd -e POSTGRES_DB=library-db -d postgres
+
+
