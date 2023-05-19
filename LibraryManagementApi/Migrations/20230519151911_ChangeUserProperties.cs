eed95d71ca365f1f188f337e0eb41eb60ba6de6b
Vlad Bulete <vladone1996@gmail.com>
2023-05-19 18:19:43 +0300

    updated migrations and database structure to current

---
+++ LibraryManagementApi/Migrations/20230519151911_ChangeUserProperties.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookLendsFlag",
+                table: "Users",
+                newName: "BookRentalOverdue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookRentalOverdue",
+                table: "Users",
+                newName: "BookLendsFlag");
+        }
+    }
+}
