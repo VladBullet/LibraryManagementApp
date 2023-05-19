1ad9c1d0c81b8466b3a46f872e6d60ac27f38f1e
Vlad Bulete <vladone1996@gmail.com>
2023-05-19 12:22:37 +0300

    add getClaimValue helper

--- LibraryManagementApi/Helpers&Extensions/Helpers.cs
+++ LibraryManagementApi/Helpers&Extensions/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryManagementApi.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -9,5 +10,10 @@ namespace LibraryManagementApi.Helpers_Extensions
 {
     public static class Helpers
     {
+        public static string GetClaimValue(ClaimsPrincipal user, string claimType)
+        {
+            var value = user.FindFirstValue(claimType);
+            return value;
+        }
     }
 }
