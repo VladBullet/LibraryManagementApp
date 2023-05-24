f1d3498eac3ba31b9ff1c78d027b4fc0fea29c12
Vlad Bulete <vlad.bulete@proplanet.com.ro>
2023-05-24 18:19:12 +0300

    fix merge

--- LibraryManagementApi/DockerCommands.ps1
+++ LibraryManagementApi/DockerCommands.ps1
@@ -1,4 +1,5 @@
 ﻿# Run postgresql docker 
-docker run --name postgres-library-db -p 5455:5432 -e POSTGRES_USER=postgresUser -e POSTGRES_PASSWORD=	 -e POSTGRES_DB=library-db -d postgres
+docker run --name postgres-library-db -p 5455:5432 -e POSTGRES_USER=postgresUser -e POSTGRES_PASSWORD=my5ecr3tqa55w0rd -e POSTGRES_DB=library-db -d postgres
 
-
+# Build app
+docker build -f LibraryManagementApi/Dockerfile .
